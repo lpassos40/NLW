27df0e8f43a183de9accf12d854c5c4e90aabd53
lpassos40 <lucaspassos2005@gmail.com>
2022-09-22 15:48:27 -0300

    Content changed

--- esports/web/src/App.tsx
+++ esports/web/src/App.tsx
@@ -1,24 +1,7 @@
-// Componentes / Prorpiedades
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return(
-  <button>
-    {props.title}
-  </button>
-  )
-}
-
 function App() {
 return (
   <div>
-    <Button title="Send 1" />
-    <Button title="Send 2" />
-    <Button title="Send 3" />
-    <Button title="Hello World" />
+
   </div>
 )
 }
