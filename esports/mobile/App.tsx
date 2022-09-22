fa89ff5b01e028f6579da0058b9ccf67200f3a08
lpassos40 <lucaspassos2005@gmail.com>
2022-09-21 21:42:28 -0300

    Added mobile version

---
+++ esports/mobile/App.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button title="Send 1"></Button>
+      <Button title="Send 2"></Button>
+      <Button title="Send 3"></Button>
+      <Button title="Hello World"></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
